2f1e771fc77c37330352c8225c36aeba2ac7044b
Drew <dmb_2000@comcast.net>
2018-05-23 09:48:04 -0400

    finished demo
    added remaining sqaures

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -3,4 +3,7 @@ SCHEDULER.every '2s' do
   send_event('mph', { current: rand(30..55) })
   send_event('temp', { current: rand(140..210) })
   send_event('lap', { current: rand(1..11) })
+  send_event('lap', { current: rand(1..11) }) 
+  send_event('time', { current: Time.new.strftime("%H:%M:%S") })
+  send_event('delta', {current: "+0.0" + rand(5).to_s})
 end
